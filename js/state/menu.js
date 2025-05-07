9f1afafdc219be3d8bfc41606e0800c6ee1e9ebc
tomcomtang <364786053@qq.com>
2025-05-07 17:52:50 +0800

    Add files via upload

--- js/state/menu.js
+++ js/state/menu.js
@@ -1,24 +1,24 @@
 "use strict";
 window.sjm.state.menu = {
-	preload: function(){
-		
+	preload: function () {
+
 	},
-	
-	create: function(){
+
+	create: function () {
 		// you can create menu group in map editor and load it like this:
 		// mt.create("menu");
-		//背景
+		// Background
 		mt.create('bg');
-		//游戏开始按钮
-		this.startBtn=mt.create('btnStart');
-		this.startBtn.inputEnabled=true;
-		this.startBtn.events.onInputDown.add(this.play,this);
+		// Game Start Button
+		this.startBtn = mt.create('btnStart');
+		this.startBtn.inputEnabled = true;
+		this.startBtn.events.onInputDown.add(this.play, this);
 	},
-	
-	update: function(){
-		
+
+	update: function () {
+
 	},
-	play:function(){
+	play: function () {
 		this.game.state.start('play');
 	}
 };
